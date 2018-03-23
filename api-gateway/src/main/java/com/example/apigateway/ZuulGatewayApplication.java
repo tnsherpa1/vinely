99822700405aa6e652b24a6d8ac4201174045e8d
tnsherpa1 <tnsherpa1@gmail.com>
2018-03-23 13:37:57 -0700

    builds zool api gateway

--- api-gateway/src/main/java/com/example/apigateway/ZuulGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ZuulGatewayApplication.java
@@ -2,11 +2,14 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
-public class DemoApplication {
+public class ZuulGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ZuulGatewayApplication.class, args);
 	}
 }
+
