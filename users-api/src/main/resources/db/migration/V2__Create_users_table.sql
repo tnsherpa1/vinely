74e112a612e2d60a79ee1fce7c75985cd4d71d36
tnsherpa1 <tnsherpa1@gmail.com>
2018-03-25 12:14:05 -0700

    successfully seeds users to db

---
+++ users-api/src/main/resources/db/migration/V2__Create_users_table.sql
@@ -0,0 +1,6 @@
+create table USERS (
+    ID serial,
+    USER_NAME varchar(50) not null,
+    FIRST_NAME varchar(50),
+    LAST_NAME varchar(50)
+);
---
+++ users-api/src/main/resources/db/migration/afterMigrate.sql
@@ -0,0 +1,2 @@
+--First User
+insert into users(user_name, first_name, last_name) values('aschwarzenegger', 'Arnold', 'schwarzenegger')
