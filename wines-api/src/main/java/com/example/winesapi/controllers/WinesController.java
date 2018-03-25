3259c45f4a078084015d6855628543d7c2757837
tnsherpa1 <tnsherpa1@gmail.com>
2018-03-25 11:04:59 -0700

    successfully updates a wine via put request

--- wines-api/src/main/java/com/example/winesapi/controllers/WinesController.java
+++ wines-api/src/main/java/com/example/winesapi/controllers/WinesController.java
@@ -21,4 +21,16 @@ public class WinesController {
     public Wine createNewWine(@RequestBody Wine newWine) {
         return wineRepository.save(newWine);
     }
+
+    @PutMapping("/wines/{wineId}")
+    public Wine updateWineById(@PathVariable Long wineId, @RequestBody Wine wineRequest) {
+        Wine wineFromDb = wineRepository.findOne(wineId);
+        wineFromDb.setTitle(wineRequest.getTitle());
+        wineFromDb.setRegion(wineRequest.getRegion());
+        wineFromDb.setKind(wineRequest.getKind());
+        wineFromDb.setPrice(wineRequest.getPrice());
+        wineFromDb.setQty(wineRequest.getQty());
+        wineFromDb.setDescription(wineFromDb.getDescription());
+        return wineRepository.save(wineFromDb);
+    }
 }
