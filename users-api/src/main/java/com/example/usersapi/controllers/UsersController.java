8d343c35f05bd983be0ec43b7f248090f0bd78fd
tnsherpa1 <tnsherpa1@gmail.com>
2018-03-25 13:11:46 -0700

    successfully creates, updates and deletes a user via http request

--- users-api/src/main/java/com/example/usersapi/controllers/UsersController.java
+++ users-api/src/main/java/com/example/usersapi/controllers/UsersController.java
@@ -3,8 +3,8 @@ package com.example.usersapi.controllers;
 import com.example.usersapi.beans.User;
 import com.example.usersapi.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UsersController {
@@ -14,5 +14,26 @@ public class UsersController {
     @GetMapping("/users")
     public Iterable<User> findAllUsers() {
         return userRepository.findAll();
+    }
+
+    @PostMapping("/users")
+    public User createNewUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
+
+    @PutMapping("/users/{userId}")
+    public User updateUserById(@PathVariable Long userId, @RequestBody User userRequest) {
+        User userFromDb = userRepository.findOne(userId);
+        userFromDb.setUserName(userRequest.getUserName());
+        userFromDb.setFirstName(userRequest.getFirstName());
+        userFromDb.setLastName(userRequest.getLastName());
+
+        return userRepository.save(userFromDb);
+    }
+
+    @DeleteMapping("/users/{userId}")
+    public HttpStatus deleteUserById(@PathVariable Long userId) {
+        userRepository.delete(userId);
+        return HttpStatus.OK;
     }
 }
